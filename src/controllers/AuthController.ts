68d2e46bd36380a95facd3f78c3cc077e32fc1cf
reniej-14 <jeinerclash@gmail.com>
2026-03-23 20:02:33 -0600

    hasheando passwords

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,20 +1,35 @@
 import { Request, Response } from "express"
+import slug from 'slug'
 import User from "../config/models/User"
+import { hashPassword } from "../utils/auth"
 
 export class AuthController {
 
     static createAcount = async (req: Request, res: Response) => {
-        const { email } = req.body
+        try {
+            const { email, password } = req.body
 
-        const userExist = await User.findOne({email})
-        if (userExist) {
-            const error = new Error('El usuario ya está registrado')
-            return res.status(409).json({error: error.message})
+            const userExist = await User.findOne({email})
+            if (userExist) {
+                const error = new Error('Un usuario con ese email ya está registrado')
+                return res.status(409).json({error: error.message})
+            }
+
+            const handle = slug(req.body.handle, '')
+            const handleExist = await User.findOne({handle})
+            if (handleExist) {
+                const error = new Error('Nombre de usuario no disponible')
+                return res.status(409).json({error: error.message})
+            }
+
+            const user = new User(req.body)
+            user.password = await hashPassword(password)
+            user.handle = handle
+
+            await user.save()
+            res.status(201).send('Usuario agregado correctamente')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
         }
-
-        const user = new User(req.body)
-
-        await user.save()
-        res.status(201).send('Usuario agregado correctamente')
     }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import { body } from 'express-validator'
 import { AuthController } from "./controllers/AuthController";
+import { handleInputErrors } from "./middleware/validation";
 
 const router = Router()
 
 // Autenticacion y registro
-router.post('/auth/register', AuthController.createAcount)
+router.post('/auth/register', 
+    body('handle')
+        .notEmpty().withMessage('El handle no puede ir vacío'),
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacío'),
+    handleInputErrors,
+    AuthController.createAcount)
 
 export default router
