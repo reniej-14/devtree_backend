e1651e5f45dcff2a6838c8520a093a0e5c3fca27
reniej-14 <jeinerclash@gmail.com>
2026-03-26 19:56:54 -0600

    Agregando CORS

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import slug from 'slug'
-import User from "../config/models/User"
+import User from "../models/User"
 import { checkPassword, hashPassword } from "../utils/auth"
 import { check } from "express-validator"
 
